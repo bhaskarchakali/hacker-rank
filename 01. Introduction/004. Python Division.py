20df58de0cff457a583c1de110483d9e2b89f2e4
Bhaskar Chakali <115171785+bhaskarchakali@users.noreply.github.com>
2023-03-04 22:55:34 +0530

    Add files via upload

---
+++ 01. Introduction/004. Python Division.py
@@ -0,0 +1,4 @@
+# Problem: https://www.hackerrank.com/challenges/python-division/problem
+
+a, b = int(input()), int(input())
+print(a // b, a / b, sep='\n')
---
+++ 01. Introduction/005. Loops.py
@@ -0,0 +1,4 @@
+# Problem: https://www.hackerrank.com/challenges/python-loops/problem
+
+for i in range(int(input())):
+    print(i ** 2)
